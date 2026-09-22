cffe1b4b546babb4f1b55a08ca49e5a1e6809365
kevingggg-arch <kevinyauribolanos@gmail.com>
2026-09-22 10:26:08 +0200

    Add music playback functionality to script.js

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ window.onload = () => {
     setTimeout(() => {
         document.querySelector(".mensaje").classList.add("mostrar");
     }, 6000);
+
+    const musica = document.getElementById("musica");
+    const boton = document.getElementById("botonMusica");
+
+    musica.volume = 0.5;
+
+    musica.play().then(() => {
+        boton.style.display = "none";
+    }).catch(() => {
+        boton.style.display = "block";
+    });
+
+    boton.addEventListener("click", () => {
+        musica.play();
+        boton.style.display = "none";
+    });
 };
